1288e0041f2d0c0fe62f6482e519ec6298c48e9c
hypeofpipe <hypeofpipe@gmail.com>
2019-09-12 22:07:13 +0200

    Put all commits in one
    Why do I need to bother myself

---
+++ CVGenTests/CVGeneratorTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CVGen;
+
+namespace CVGenTests
+{
+    [TestClass]
+    public class CVGeneratorTests
+    {
+        [TestMethod]
+        public void HTMLToPDFTest()
+        {
+            var html = "<div>{{somedata}}</div>";
+            var cvGen = new CvGenerator();
+            Assert.IsInstanceOfType(cvGen.HTMLToPDF(html), typeof(IronPdf.PdfDocument));
+        }
+        [TestMethod]
+        public void RenderCVTest()
+        {
+            var html = "<div>{{INPUTDATA}}</div>";
+            var data = new
+            {
+                INPUTDATA = "thatstestdata"
+            };
+            var cvGen = new CvGenerator();
+
+            var result = cvGen.RenderCV(data, html);
+
+            Assert.AreEqual(result, "<div>thatstestdata</div>");
+        }
+    }
+}
